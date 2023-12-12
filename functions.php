17957f05453f646810438d3c51b5eb653093bff0
Ambuj Kumar <ambujkumar22@gmail.com>
2023-12-12 12:17:14 +0530

    first commit

---
+++ functions.php
@@ -0,0 +1,44 @@
+<?php
+
+$books = [
+    [
+        'name' => 'Books',
+        'author' => 'ambuj kumar',
+        'releaseYear' => 1997
+    ],
+    [
+        'name' => 'Books 2',
+        'author' => 'krishan kumar',
+        'releaseYear' => 1996
+    ]
+];
+
+$filter = function($items, $function){
+    if(!empty($items) && is_array($items)){
+        $filteredItems = [];
+        foreach($items as $item){
+            if($function($item)){
+                $filteredItems[] = $item;
+            }
+        }
+        
+        return $filteredItems;
+    }
+};
+
+$filteredBooks = array_filter($books, function($books){
+    return $books['releaseYear'] >= 1996;
+});
+
+function dd($variable){
+    echo '<pre>';
+    echo var_dump($variable);
+    echo '</pre>';
+
+    die();
+}
+
+function urlIs($value){
+    $path = parse_url($_SERVER['REQUEST_URI'])['path'];
+    return $path == $value;
+}
